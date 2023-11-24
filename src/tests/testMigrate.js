ea2b834fe4a9c140ae8f9df6b18a55fa09ff5985
marco perez <mp4667596@gmail.com>
2023-11-24 12:26:51 -0300

    first comit

--- src/tests/testMigrate.js
+++ src/tests/testMigrate.js
@@ -1,8 +1,22 @@
+const app = require('../app');
 const sequelize = require('../utils/connection');
+const request = require('supertest');
 
 const main = async() => {
     try{
         // Acciones a ejecutar antes de los tests
+        const user = {
+            firstName: "test",
+            lastName: "test",
+            email: "test@gmai.com",
+            password: "test123",
+            phone: "123456158"
+        }
+        await request(app).post('/users').send(user)
+
+
+
+
         sequelize.sync();
         
         process.exit();
